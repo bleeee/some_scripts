cfa8c9f30686c79cdbfb4070b3478c6631dafe28
bleeee <34237584+bleeee@users.noreply.github.com>
2024-06-23 00:44:35 +0800

    Update pi4b20240622.sh

--- pi4b20240622.sh
+++ pi4b20240622.sh
@@ -1,5 +1,5 @@
 apt update -y && apt upgrade -y
-apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio vlc wireguard ffmpeg traceroute dnsutils -y
+apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio vlc wireguard ffmpeg udhcpc traceroute dnsutils -y
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
 wget http://www.waveshare.net/w/upload/0/06/Fan_HAT.7z
 tar zxvf bcm2835-1.60.tar.gz
@@ -42,6 +42,9 @@ sed -i "s|^Endpoint = .*|Endpoint = $Endpoint|" /etc/wireguard/wg0.conf
 systemctl daemon-reload
 systemctl enable wg-quick@wg0
 systemctl enable stream.service
-CONNECTION_NAME=$(nmcli connection show | grep usb0 | awk '{$NF=""; $(NF-1)=""; $(NF-2)=""; print $0}' | sed 's/[[:space:]]*$//')
-nmcli connection modify "$CONNECTION_NAME" ipv4.dns "1.1.1.1"
-nmcli connection modify "$CONNECTION_NAME" ipv4.ignore-auto-dns yes
+#CONNECTION_NAME=$(nmcli connection show | grep usb0 | awk '{$NF=""; $(NF-1)=""; $(NF-2)=""; print $0}' | sed 's/[[:space:]]*$//')
+#nmcli connection modify "$CONNECTION_NAME" ipv4.dns "1.1.1.1"
+#nmcli connection modify "$CONNECTION_NAME" ipv4.ignore-auto-dns yes
+dhclient -v usb0
+udhcpc -i usb0
+route add -net 0.0.0.0 usb0
