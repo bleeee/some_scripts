dd9b4f677229fc82f6398adf303367cde707cc67
bleeee <34237584+bleeee@users.noreply.github.com>
2024-06-22 20:17:07 +0800

    Update pi4b20240622.sh

--- pi4b20240622.sh
+++ pi4b20240622.sh
@@ -1,5 +1,5 @@
 apt update -y && apt upgrade -y
-apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio
+apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio -y
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
 wget http://www.waveshare.net/w/upload/0/06/Fan_HAT.7z
 tar zxvf bcm2835-1.60.tar.gz
