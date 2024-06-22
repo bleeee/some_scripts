d1e40f19fd37938f7b75d1378bd0faeafca791a0
bleeee <34237584+bleeee@users.noreply.github.com>
2024-06-22 22:06:45 +0800

    Update pi4b20240622.sh

--- pi4b20240622.sh
+++ pi4b20240622.sh
@@ -1,5 +1,5 @@
 apt update -y && apt upgrade -y
-apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio vlc -y
+apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio vlc wireguard -y
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
 wget http://www.waveshare.net/w/upload/0/06/Fan_HAT.7z
 tar zxvf bcm2835-1.60.tar.gz
@@ -19,3 +19,20 @@ tar zxvf mediamtx_v1.8.3_linux_arm64v8.tar.gz
 wget https://github.com/bleeee/some_scripts/raw/master/stream.sh.live
 wget https://github.com/bleeee/some_scripts/raw/master/stream.sh.debug
 cp stream.sh.live stream.sh
+chmod +x stream.sh
+chmod +x stream.sh.live
+chmod +x stream.sh.debug
+wget -O /etc/systemd/system/stream.service https://github.com/bleeee/some_scripts/raw/master/stream.service
+wget -O /etc/wireguard/wg0.conf https://github.com/bleeee/some_scripts/raw/master/wg0.conf
+read -p "请输入私钥: " PrivateKey 
+read -p "请输入公钥: " PublicKey 
+read -p "请输入PSK: " PresharedKey 
+read -p "请输入地址: " Endpoint
+sed -i 's/^aa=$/aa=xx/' /etc/wireguard/wg0.conf
+sed -i 's/^aa=$/aa=xx/' /etc/wireguard/wg0.conf
+sed -i 's/^aa=$/aa=xx/' /etc/wireguard/wg0.conf
+sed -i 's/^aa=$/aa=xx/' /etc/wireguard/wg0.conf
+
+systemctl daemon-reload
+systemctl start stream.service
+systemctl enable stream.service
