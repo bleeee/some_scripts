3d74f5434b14bc9812825ccad24cf3b7afb08de8
bleeee <34237584+bleeee@users.noreply.github.com>
2024-06-22 21:09:39 +0800

    Update pi4b20240622.sh

--- pi4b20240622.sh
+++ pi4b20240622.sh
@@ -1,5 +1,5 @@
 apt update -y && apt upgrade -y
-apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio -y
+apt install minicom p7zip-full python3-smbus python3-pillow python3-numpy python3-rpi.gpio vlc -y
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
 wget http://www.waveshare.net/w/upload/0/06/Fan_HAT.7z
 tar zxvf bcm2835-1.60.tar.gz
@@ -16,3 +16,6 @@ mkdir carcam
 cd carcam
 https://github.com/bluenviron/mediamtx/releases/download/v1.8.3/mediamtx_v1.8.3_linux_arm64v8.tar.gz
 tar zxvf mediamtx_v1.8.3_linux_arm64v8.tar.gz
+wget https://github.com/bleeee/some_scripts/raw/master/stream.sh.live
+wget https://github.com/bleeee/some_scripts/raw/master/stream.sh.debug
+cp stream.sh.live stream.sh
